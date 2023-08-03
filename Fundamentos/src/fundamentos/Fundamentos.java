4d8a09802aa518af178d87b42432e83efe7b4459
Amanda <amanda.leme@aluno.senai.br>
2023-08-03 14:09:31 -0300

    enviando arquivos

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -10,9 +10,6 @@ package fundamentos;
  */
 public class Fundamentos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Declaração de variáveis de tipos primitivos
         String nome = "Amanda";
