dd663044ad53d737b2317c61dba7fa126f3909d8
amanda23z <amanda.leme@aluno.senai.br>
2023-08-17 10:19:22 -0300

    Aprendendo enumerações em Java

---
+++ Fundamentos/src/enumeracao/TipoUsuarioEnum.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao;
+
+import javax.naming.CompoundName;
+import javax.swing.plaf.metal.MetalIconFactory;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum TipoUsuarioEnum {
+    COMUM, TECNICO, ADMINISTRADOR 
+    
+    
+}
