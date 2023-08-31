c04b631c3369b307676613c98d7e3b4bff1ee1ec
amanda23z <amanda.leme@aluno.senai.br>
2023-08-31 09:25:45 -0300

    Enviando exemplos de sobrecargas e métodos construtores

---
+++ Orientacaoobjetos/src/orientacaoobjetos/Cliente.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package orientacaoobjetos;
+
+public class Cliente {
+    private String nome;
+    private String email;
+
+    public Cliente(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nome=" + nome + ", email=" + email + '}';
+    }
+    
+    
+    
+}
